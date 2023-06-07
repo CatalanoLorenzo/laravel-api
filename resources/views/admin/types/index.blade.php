7dd7072fa06def0bcfd27ab0957701f0110ed2cd
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-06-07 22:48:51 +0200

    add modal for destroy function

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -22,7 +22,6 @@
                     table-primary
                     align-middle">
                     <thead class="table-light">
-                        <caption>Types</caption>
                         <tr>
                             <th>ID</th>
                             <th>Cover</th>
@@ -41,11 +40,46 @@
                             <td>
                                 <a class="btn btn-primary" href="{{route('admin.types.show' , $type )}}" role="button">VIEW</a>
                                 <a class="btn btn-primary" href="{{route('admin.types.edit' , $type )}}" role="button">Edit</a>
-                                <form action="{{route('admin.types.destroy',$type)}}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
+
+
+
+
+                                <!-- Modal trigger button -->
+                                <button type="button" class="btn btn-danger " data-bs-toggle="modal" data-bs-target="#{{$type->id}}">
+                                    Delete
+                                </button>
+
+                                <!-- Modal Body -->
+                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                <div class="modal fade" id="{{$type->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="{{$type->id}}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="{{$type->id}}">{{$type->name}}</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Body
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Back</button>
+                                                <form action="{{route('admin.types.destroy',$type)}}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <!-- Optional: Place to the bottom of scripts -->
+                                <script>
+                                    const myModal = new bootstrap.Modal(document.getElementById('{{$type->id}}'), options)
+                                </script>
+
+
                             </td>
                         </tr>
 
@@ -64,26 +98,26 @@
 <div class="container">
     <div class="row">
         <div class="col">
-           
-               @if ($singletype)
-               <div class="card">
+            <h2>Type Select</h2>
+            @if ($singletype)
+            <div class="card">
                 <img class="card-img-top" src="{{$singletype->cover}}" alt="{{$singletype->name}}">
                 <div class="card-body">
                     <h4 class="card-title text-center">{{$singletype->name}}</h4>
                     <p class="card-text text-center">{{$singletype->slug}}</p>
                 </div>
-               </div>
-               @else
-                <div class="alert alert-info" role="alert">
-                    <strong>Select Type</strong>
-                </div>
-                
-               @endif
-               
+            </div>
+            @else
+            <div class="alert alert-info" role="alert">
+                <strong>Select Type</strong>
+            </div>
 
-           
-               
-           
+            @endif
+
+
+
+
+
         </div>
     </div>
 </div>
